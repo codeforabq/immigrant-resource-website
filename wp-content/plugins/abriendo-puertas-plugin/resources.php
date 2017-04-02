edc65bdd09395bf7067e4b19df0f1f353bf03cbf
Monica Alvarez <mmalvar13@gmail.com>
2017-04-02 16:38:52 -0600

    added cmb2 custom fields html

--- wp-content/plugins/abriendo-puertas-plugin/resources.php
+++ wp-content/plugins/abriendo-puertas-plugin/resources.php
@@ -2,18 +2,18 @@
 /**
  * Hook in and add a metabox that only appears on the 'Resources' page
  */
-if(!function_exists('zfw_register_services_metabox')) {
-    function zfw_register_services_metabox() {
-        $prefix = '_zfw_services_metabox_';
+if(!function_exists('ap_register_resources_metabox')) {
+    function ap_register_resources_metabox() {
+        $prefix = '_ap_resources_metabox_';
 
         /**
          * Metabox to be displayed on a single page ID
          */
         $cmb = new_cmb2_box(array(
             'id' => $prefix . 'metabox',
-            'title' => 'Services',
+            'title' => 'Resources',
             'object_types' => array('page'), //Post type
-            'show_on' => array('key' => 'page-template', 'value' => 'services.php'), //Find real post/page number later
+            'show_on' => array('key' => 'page-template', 'value' => 'resources.php'), //Find real post/page number later
             'show_names' => true, //show field names on the left
             'context' => 'normal',
             'priority' => 'high',
@@ -21,13 +21,13 @@ if(!function_exists('zfw_register_services_metabox')) {
 
         //$group_field_id is the field id string, so in this case: $prefix. 'demo'
         $group_field_id = $cmb->add_field(array(
-            'id' => $prefix . 'Service',
+            'id' => $prefix . 'Resources',
             'type' => 'group',
-            'description' => __('Enter Service', 'cmb2'),
+            'description' => __('Enter Resource', 'cmb2'),
             'options' => array(
                 'group_title' => __('Entry{#}', 'cmb2'),
-                'add_button' => __('Add Another Service', 'cmb2'),
-                'remove_button' => __('Remove Service', 'cmb2'),
+                'add_button' => __('Add Another Resource', 'cmb2'),
+                'remove_button' => __('Remove Resource', 'cmb2'),
                 'sortable' => true,
                 'repeatable' => true,
             ),
@@ -35,14 +35,15 @@ if(!function_exists('zfw_register_services_metabox')) {
 
         $cmb->add_group_field($group_field_id, array(
             'name' => 'Name',
-            'description' => 'Name of Service',
+            'description' => 'Name of Resource',
             'id' => 'name',
             'type' => 'text',
         ));
         $cmb->add_group_field($group_field_id, array(
-            'name' => 'Service',
-            'id' => 'service',
-            'type' => 'textarea_small',
+            'name' => 'Resource',
+            'description' => 'Description of Resource',
+            'id' => 'resource',
+            'type' => 'textarea',
         ));
         $cmb->add_group_field($group_field_id, array(
             'name' => 'Photo',
@@ -50,20 +51,11 @@ if(!function_exists('zfw_register_services_metabox')) {
             'type' => 'file',
         ));
         $cmb->add_group_field($group_field_id, array(
-            'name' => 'Woocommerce Bookings Link',
-            'id' => 'bookings',
-            'type' => 'text',
-        ));
-        $cmb->add_group_field($group_field_id, array(
             'name' => 'Learn More',
             'id' => 'learn-more',
             'type' => 'text',
         ));
-        $cmb->add_group_field($group_field_id, array(
-            'name' => 'Testimonials',
-            'id' => 'services-testimonial',
-            'type' => 'wysiwyg',
-        ));
+
     }
 }
-add_action('cmb2_admin_init', 'zfw_register_services_metabox');
+add_action('cmb2_admin_init', 'ap_register_resources_metabox');
--- wp-content/themes/one-pager-genesis-master/resources.php
+++ wp-content/themes/one-pager-genesis-master/resources.php
@@ -1,5 +1,78 @@
 <?php
 /**
  * Template Name: Resources
-**/
+ **/
 
+add_action('genesis_entry_content', 'add_resources');
+function add_resources()
+{
+    $meta = get_post_meta(get_the_ID(), '_ap_resources_metabox_Resources', true);
+    $count = 0;
+    foreach ((array)$meta as $key => $entry) {
+        $count++;
+        if ($count % 2 === 1) {
+            ?>
+            <div class="resource-section one-half first">
+                <div class="resource-title">
+
+                </div>
+                <div class="resource-image">
+                    <?php
+                    if (isset($entry['photo'])) {
+                        echo wp_get_attachment_image_src($entry['photo_id'], 'full')[0];
+                    }
+                    ?>
+                </div>
+                <div class="resource-description">
+                    <?php
+                    if (isset($entry['resource'])) {
+                        echo($entry['resource']);
+                    }
+                    ?>
+                </div>
+                <div class="resource-link">
+                    <?php
+                    if (isset($entry['learn-more'])) {
+                        echo($entry['learn-more']);
+                    }
+                    ?>
+                </div>
+            </div>
+            <?php
+
+        } else {
+            ?>
+            <div class="resource-section one-half">
+                <div class="resource-title">
+
+                </div>
+                <div class="resource-image">
+                    <?php
+                    if (isset($entry['photo'])) {
+                        echo wp_get_attachment_image_src($entry['photo_id'], 'full')[0];
+                    }
+                    ?>
+                </div>
+                <div class="resource-description">
+                    <?php
+                    if (isset($entry['resource'])) {
+                        echo($entry['resource']);
+                    }
+                    ?>
+                </div>
+                <div class="resource-link">
+                    <?php
+                    if (isset($entry['learn-more'])) {
+                        echo($entry['learn-more']);
+                    }
+                    ?>
+                </div>
+            </div>
+            <?php
+        }
+    }
+}
+
+
+
+genesis();
