c61d0a6d2d4dab7993e3aa491d06dc9d103192c8
Monica Alvarez <mmalvar13@gmail.com>
2017-04-06 01:23:24 -0600

    removed commented out code from resources template

--- wp-content/themes/one-pager-genesis-master/resources.php
+++ wp-content/themes/one-pager-genesis-master/resources.php
@@ -3,93 +3,9 @@
  * Template Name: Resources
  **/
 
-//add_action('genesis_entry_content', 'add_resources');
-//function add_resources()
-//{
-//    $meta = get_post_meta(get_the_ID(), '_ap_resources_metabox_Resources', true);
-//    $count = 0;
-//    foreach ((array)$meta as $key => $entry) {
-//        $count++;
-//        if ($count === 1 || $count % 2 === 1) {
-//            ?>
-    <!--            <div class="resource-section one-half first">-->
-    <!--            --><?php
-//        } else {
-//            ?>
-    <!--            <div class="resource-section one-half">-->
-    <!--            --><?php
-//        }
-//        ?>
-    <!--        <div class="resource-inner">-->
-    <!--            <div class="resource-title">-->
-    <!--                --><?php
-//                if (isset($entry['name'])) {
-//                    echo($entry['name']);
-//                }
-//                ?>
-    <!---->
-    <!--            </div>-->
-    <!--            <div class="resource-image">-->
-    <!--                --><?php
-//                if (isset($entry['photo'])) {
-//                    echo wp_get_attachment_image($entry['photo_id'], 'full', "", array("class" => "alignright"));
-//                }
-//                ?>
-    <!--            </div>-->
-    <!---->
-    <!---->
-    <!--            <div class="resource-description">-->
-    <!--                --><?php
-//                if (isset($entry['resource'])) {
-//                    echo($entry['resource']);
-//                }
-//                ?>
-    <!--            </div>-->
-    <!---->
-    <!---->
-    <!--            <div class="resource-contact-info">-->
-    <!--                <div class="resource-address">-->
-    <!--                    --><?php
-//                    if (isset($entry['address'])) {
-//                        echo '<span class="label">Address: </span>' . ($entry['address']);
-//                    }
-//                    ?>
-    <!--                </div>-->
-    <!--                <div class="resource-phone">-->
-    <!--                    --><?php
-//                    if (isset($entry['phone-number'])) {
-//                        echo '<span class="label">Phone: </span>' . ($entry['phone-number']);
-//                    }
-//                    ?>
-    <!--                </div>-->
-    <!--                <div class="resource-email">-->
-    <!--                    --><?php
-//                    if (isset($entry['email'])) {
-//                        echo '<span class="label">E-mail: </span>' . ($entry['email']);
-//                    }
-//                    ?>
-    <!--                </div>-->
-    <!--            </div>-->
-    <!---->
-    <!---->
-    <!--            <div class="resource-link">-->
-    <!--                --><?php
-//                if (isset($entry['learn-more']) && $entry['learn-more'] != '') {
-//                    echo '<a class="button" href="' . $entry['learn-more'] . ' " target="_blank">';
-//                    echo 'Learn More</a>';
-//                }
-//                ?>
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        </div>-->
-    <!--        --><?php
-//
-//    }
-//
-//}
 
-add_action('genesis_entry_content', 'add_resource');
-function add_resource()
+add_action('genesis_entry_content', 'add_resources');
+function add_resources()
 {
     $meta = get_post_meta(get_the_ID(), '_ap_resources_metabox_Resources', true);
     foreach ((array)$meta as $key => $entry) {
