137e13351a08c24e22d8e0384be72cbe829a3b03
Monica Alvarez <mmalvar13@gmail.com>
2017-04-05 23:16:08 -0600

    added font awesome stylesheet and changed front page styles

--- wp-content/themes/one-pager-genesis-master/functions.php
+++ wp-content/themes/one-pager-genesis-master/functions.php
@@ -40,6 +40,12 @@ add_action( 'wp_enqueue_scripts', 'theme_name_scriptsss' );
 
 
 
+//enqueues our external font awesome stylesheet
+function enqueue_font_awesome_stylesheets(){
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+}
+add_action('wp_enqueue_scripts','enqueue_font_awesome_stylesheets');
+
 
 
 // One-Pager specific code below
